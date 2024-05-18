5980e9b1f65d156c91d8721f2b705b42bc6bd31b
Dilini Shashikala Gnanarathne <shashikaladilini11@gmail.com>
2024-05-18 23:40:25 +0530

    doctor cant see user account and user cant see doctor dashbord

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -8,7 +8,7 @@ import Doctors from '../pages/Doctors/Doctors'
 import DoctorDetails from '../pages/Doctors/DoctorDetails'
 import MyAccount from "../Dashboard/user-account/MyAccount"
 import Dashboard from "../Dashboard/doctor-account/Dashboard"
-//import ProtectedRoute from './ProtectedRoute'
+import ProtectedRoute from './ProtectedRoute'
 import {Routes, Route} from 'react-router-dom'
 
 
@@ -22,11 +22,8 @@ const Routers = () => {
     <Route path="/register" element={<Signup />} />
     <Route path="/contact" element={<Contact />} />
     <Route path="/services" element={<Service />} />
-    {/* <Route path="/users/profile/me" element={<ProtectedRoute allowedRoles={["patient"]}><MyAccount /></ProtectedRoute>} />
+    <Route path="/users/profile/me" element={<ProtectedRoute allowedRoles={["patient"]}><MyAccount /></ProtectedRoute>} />
     <Route path="/doctors/profile/me" element={<ProtectedRoute allowedRoles={["doctor"]}><Dashboard /></ProtectedRoute>} />
-   */}
-     <Route path="/users/profile/me"element= {<MyAccount />} />
-    <Route path="/doctors/profile/me" element={<Dashboard />} />
   
   </Routes>
     
